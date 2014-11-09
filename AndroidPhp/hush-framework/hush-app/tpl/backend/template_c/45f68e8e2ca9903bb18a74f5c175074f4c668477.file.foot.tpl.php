be9a597fa61e711e2e4ee0adacac6ec9154e993a
Locosoft <locosoft1986@gmail.com>
2014-11-09 18:03:25 +0800

    add android PHP add tomcat

---
+++ AndroidPhp/hush-framework/hush-app/tpl/backend/template_c/45f68e8e2ca9903bb18a74f5c175074f4c668477.file.foot.tpl.php
@@ -0,0 +1,22 @@
+<?php /* Smarty version Smarty3-b8, created on 2014-03-21 14:40:48
+         compiled from "D:\workspace\hush-framework\hush-app\tpl\backend\template\frame/foot.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:15019532bdef0c589c7-16128134%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '45f68e8e2ca9903bb18a74f5c175074f4c668477' => 
+    array (
+      0 => 'D:\\workspace\\hush-framework\\hush-app\\tpl\\backend\\template\\frame/foot.tpl',
+      1 => 1357552552,
+    ),
+  ),
+  'nocache_hash' => '15019532bdef0c589c7-16128134',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+)); /*/%%SmartyHeaderCode%%*/?>
+</div>
+
+</body>
+</html>
