12d687aad0e7b49c58b06d10e433a48519e11b7c
locosoft1986 <locosoft1986@gmail.com>
2014-12-11 16:04:26 +0800

    Javascript test

--- YesOrNoProject/Client/assets/www/JS/globalEditorJS.js
+++ YesOrNoProject/Client/assets/www/JS/globalEditorJS.js
@@ -1,3 +1,35 @@
+
+    function fetchAllCSS(a) {
+        var sheets = document.styleSheets, o = {};
+        for (var i in sheets) {
+            var rules = sheets[i].rules || sheets[i].cssRules;
+            for (var r in rules) {
+                if (a.is(rules[r].selectorText)) {
+                    o = $.extend(o, _css2json(rules[r].style), _css2json(a.attr('style')));
+                }
+            }
+        }
+        return o;
+    }
+
+    function _css2json(css) {
+        var s = {};
+        if (!css) return s;
+        if (css instanceof CSSStyleDeclaration) {
+            for (var i in css) {
+                if ((css[i]).toLowerCase) {
+                    s[(css[i]).toLowerCase()] = (css[css[i]]);
+                }
+            }
+        } else if (typeof css == "string") {
+            css = css.split("; ");
+            for (var i in css) {
+                var l = css[i].split(": ");
+                s[l[0].toLowerCase()] = (l[1]);
+            }
+        }
+        return s;
+    }
     //The map use string as key and value
     //var gElement2idMap = new Object();
     function registerInteractElement(element, id, type)
@@ -9,6 +41,17 @@
         if(opElement !== undefined && valIdElement !== undefined)
         {
             //checkJSInterface = (JSInterface !== undefined);
+            if (type == 3)
+            {
+                $d = $(id)[0].contentWindow.document;
+                $d.designMode="on";
+                $d.contentEditable= true;
+                $d.open();
+                $d.close();
+                
+                var style = fetchAllCSS(valIdElement);
+                $(id).contents().find('body').css(style);
+            }
             if (checkJSInterface)
             {
                 JSInterface.setType(type);
@@ -20,6 +63,11 @@
                     case 2://music bgm
                         {
                             valIdElement.attr('src', strElementValue);
+                        }
+                        break;
+                    case 3:
+                        {
+                            valIdElement.contents().find('body').text(strElementValue);
                         }
                         break;
                     case 0:
---
+++ YesOrNoProject/Client/assets/www/JS/tpl2.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+
+    registerInteractElement('#title_container','#ptitle',0);
+    registerInteractElement('#pcontent','#pcontent',3);
+});
